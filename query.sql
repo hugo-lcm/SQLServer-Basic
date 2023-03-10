cf0763bc1572ad709e58af44da0a3695a9e5be83
Hugo Leonardo <74077027+hugo-lcm@users.noreply.github.com>
2023-03-10 10:47:11 -0300

    fix single line comments

--- query.sql
+++ query.sql
@@ -1,12 +1,12 @@
-# inserindo dados
+-- inserindo dados
 insert into FUN_FUNCIONARIOS(fun_nome, fun_sobrenome, fun_data_nascimento, fun_observacoes) values('fulano', 'silva', '1991-02-23', 'dev');
 insert into FUN_FUNCIONARIOS(fun_nome, fun_sobrenome, fun_data_nascimento, fun_observacoes) values('ciclano', 'pereira', '1991-12-21');
 insert into FUN_FUNCIONARIOS(fun_nome, fun_sobrenome, fun_data_nascimento, fun_observacoes) values('beltrano', 'oliveira', '1985-04-01');
 
-# 7.3 - entendendo o conceito de projeção e utilizando as primeiras funções T-SQL
+-- 7.3 - entendendo o conceito de projeção e utilizando as primeiras funções T-SQL
 select concat(fun_nome, ' ', fun_sobrenome) as 'nome completo' from FUN_FUNCIONARIOS;
 select fun_nome + ' ' + fun_sobrenome + ' ' + cast(fun_observacoes as varchar) as nome_completo from FUN_FUNCIONARIOS;
-# caso fun_observacoes tenha algum valor null, usando a função concat nao precisa do cast
+-- caso fun_observacoes tenha algum valor null, usando a função concat nao precisa do cast
 select fun_nome + ' ' + fun_sobrenome + ' ' + cast(isnull(fun_observacoes, '') as varchar) as nome_completo from FUN_FUNCIONARIOS;
-# ver o tipo de dados das colunas e retornar apenas a primeira linha
+-- ver o tipo de dados das colunas e retornar apenas a primeira linha
 select top 1 sql_variant_property(fun_nome + ' ' + fun_sobrenome + ' ' + cast(isnull(fun_observacoes, '') as varchar), 'BaseType') as nome_completo from FUN_FUNCIONARIOS;
